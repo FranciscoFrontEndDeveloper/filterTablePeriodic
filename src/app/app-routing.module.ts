7ead8cb75f8ff8a5d5703ae9ea1179c435cc6831
Francisco <pachonetworking@gmail.com>
2021-01-04 17:18:20 -0500

    gridmain creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GridMainComponent } from './GridMain/grid-main/grid-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '', component: GridMainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridMainComponent } from './GridMain/grid-main/grid-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridMainComponent
   ],
   imports: [
     BrowserModule,
