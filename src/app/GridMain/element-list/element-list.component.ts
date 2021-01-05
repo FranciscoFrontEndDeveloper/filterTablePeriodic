3ba9e8cbe54a1bb6dfa9c5a44c9e1f26e8842558
Francisco <pachonetworking@gmail.com>
2021-01-05 17:35:11 -0500

    componetne de listar elementos exitoso

--- src/app/GridMain/element-list/element-list.component.ts
+++ src/app/GridMain/element-list/element-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ElementsService } from 'src/app/Services/elements.service';
+import { InterfazElement } from '../../Interfases/interfaz-element';
 
 @Component({
   selector: 'app-element-list',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./element-list.component.sass']
 })
 export class ElementListComponent implements OnInit {
-
-  constructor() { }
+public elements: any = [];
+  constructor(private elementsService: ElementsService) { }
 
   ngOnInit(): void {
+    this.getAllElements();
   }
 
+
+  getAllElements(): void {
+    this.elements = this.elementsService.Elements;
+    // console.log(this.elements);
+  }
 }
---
+++ src/app/Interfases/interfaz-element.ts
@@ -0,0 +1,24 @@
+export interface InterfazElement {
+
+  atomicNumber: number;
+  symbol: string;
+  name: string;
+  atomicMass: string;
+  cpkHexColor: string;
+  electronicConfiguration: string;
+  electronegativity: number;
+  atomicRadius: number;
+  ionRadius: string;
+  vanDerWaalsRadius: number;
+  ionizationEnergy: number;
+  electronAffinity: number;
+  oxidationStates: string;
+  standardState: string;
+  bondingType: string;
+  meltingPoint: number;
+  boilingPoint: number;
+  density: number;
+  groupBlock: 'nonmetal';
+  yearDiscovered: number;
+
+}
