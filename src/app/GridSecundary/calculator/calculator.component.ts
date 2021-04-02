8138b92cce0e500e6deaea14cbe13dc251e1b6a6
Francisco Becerra Rodriguez <pachonetworking@gmail.com>
2021-04-02 00:50:53 -0500

    comparacion entre fechas funcional falta if de comparacion

--- src/app/GridSecundary/calculator/calculator.component.ts
+++ src/app/GridSecundary/calculator/calculator.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import * as dayjs from 'dayjs';
 import 'dayjs/locale/es';
 import 'dayjs/plugin/isBetween';
-
+// tslint:disable-next-line: prefer-const
+let isBetween = require('dayjs/plugin/isBetween');
+dayjs.extend(isBetween);
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
@@ -11,46 +13,36 @@ import 'dayjs/plugin/isBetween';
 export class CalculatorComponent implements OnInit {
   public title = 'calculadora de elementos';
   public dateCurrent1: {};
-  public dateCurrent2: {};
+  public dateCurrent2: any;
   public dateCurrent3: boolean;
-  public valueDataCurrent1: string | undefined;
+  public valueDataCurrent1: any;
   public dayLoadBefore: string;
   public mountLoadBefore: any;
   public yearLoadBefore: string;
   public entreDates: any;
-  public Week1: string;
+  public Week1: any;
+  public Week2: any;
+  public Week3: any;
+  public Week4: any;
+  public dateBetween: any;
   constructor() {
     this.dateCurrent1 = new Date();
     this.dateCurrent2 = dayjs().format('DD/MM/YYYY');
     this.dateCurrent3 = true;
-    this.valueDataCurrent1 = dayjs().format('YYYY-MM-DD');
     this.dayLoadBefore = dayjs().date(24).format('DD');
     this.mountLoadBefore = dayjs().format('MM');
     this.yearLoadBefore = dayjs().format('YYYY');
-    this.Week1 = dayjs().format('YYYY-MM-DD');
-    console.log(this.Week1);
-    // console.log(this.dayLoadBefore = dayjs().format('DD'));
-    // console.log(dayjs().date(24).format('DD'));
-
-
    }
 
   ngOnInit(): void {
 
     this.addZero();
+    this.weeksLoads();
   }
 
-  daytoken(): void{
-    // console.log(this.valueDataCurrent1);
-    // console.log(this.dateCurrent2);
-    if (this.dateCurrent2 === this.valueDataCurrent1) {
-      console.log('son iguales');
-    }else {
-      console.log('son diferentes');
-    }
-  }
+
   addZero(): void{
-    this.mountLoadBefore = dayjs().format('MM');
+    // this.mountLoadBefore = dayjs().format('MM');
     this.dayLoadBefore = dayjs().date(24).format('DD');
     this.dateCurrent1 = Number(this.mountLoadBefore) - 1;
     if (this.dateCurrent1 < 10) {
@@ -59,5 +51,43 @@ export class CalculatorComponent implements OnInit {
     } else {
       this.mountLoadBefore = this.mountLoadBefore;
     }
+  }
+
+  weeksLoads(): void{
+    console.log(this.valueDataCurrent1);
+    this.Week1 = dayjs(this.valueDataCurrent1).date(24).add(8, 'day').format('YYYY-MM-DD');
+    // console.log(this.Week1);
+    this.Week2 = dayjs(this.Week1).date(1).add(8, 'day').format('YYYY-MM-DD');
+    // console.log(this.Week2);
+    this.Week3 = dayjs(this.Week2).date(9).add(8, 'day').format('YYYY-MM-DD');
+    // console.log(this.Week3);
+    this.Week4 = dayjs(this.Week3).date(17).add(7, 'day').format('YYYY-MM-DD');
+    // console.log(this.Week4);
+  }
+
+  daytoken(): void{
+    console.log(this.valueDataCurrent1);
+    console.log(this.dateCurrent2);
+
+    this.dateBetween = dayjs(this.dateCurrent2).isBetween(this.valueDataCurrent1, dayjs(this.Week1));
+    console.log(this.dateBetween);
+
+    // if (this.dateCurrent2 === this.valueDataCurrent1) {
+    //   console.log('multiplicar por 1');
+    // }else {
+    //   console.log('multiplicar por 2');
+    // }
+
+    // switch (this.dateCurrent2) {
+    //   case (this.dateCurrent2 === this.Week1):
+    //     console.log(this.dateCurrent2);
+    //     console.log(this.Week1);
+    //     console.log('multiplicar por 1');
+    //     break;
+
+    //   default:
+    //     console.log('sin fecha');
+    //     break;
+    // }
   }
 }
